cd54eddcecf07288a0250c6b0d0830ba5e11a25b
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2005-10-13 10:33:56 +0000

    COM: Draft.

--- trunk/cbear.berlios.de/com/draft.cpp
+++ trunk/cbear.berlios.de/com/draft.cpp
@@ -1,22 +1,18 @@
-class IUnknown
+/*
+struct IUnknown
 {
-public:
-	virtual int AddRef() = 0;
-	virtual int Release() = 0;
-	virtual void *QueryInterface(int Uuid) = 0;
+	virtual void QueryInterface() = 0;
 };
 
 //
 
-class A: public IUnknown
+struct A: IUnknown
 {
-public:
 	virtual int F() = 0;
 };
 
-class B: public IUnknown
+struct B: IUnknown
 {
-public:
 	virtual int F() = 0;
 };
 
@@ -25,39 +21,32 @@ public:
 class Base: public IUnknown
 {
 public:
-	virtual int AddRef() { return 0; }
-	virtual int Release() { return 0; }
-	virtual void *QueryInterface(int Uuid) { return 0; }
-protected:
-	void add_interface(int Uuid, void *P) {}
+	void query_interface() {}
 };
 
-template<class T>
-class WrapBase: public T, public virtual Base
+template<class T, class TB = T>
+class Wrap: public TB, public virtual Base
 {
 public:
-	WrapBase() { Base::add_interface(0, static_cast<T *>(this)); }
+	void QueryInterface() { return Base::query_interface(); }
 };
-
-template<class T>
-class Wrap: public WrapBase<T> {};
 
 //
 
-template<>
-class Wrap<A>: public WrapBase<A>
+template<class TB>
+class Wrap<A, TB>: public Wrap<IUnknown, TB>
 {
 public:
 	virtual int A_F() = 0;
-	int A() { return A_F(); }
+	int F() { return A_F(); }
 };
 
-template<>
-class Wrap<B>: public WrapBase<B>
+template<class TB>
+class Wrap<B, TB>: public Wrap<IUnknown, TB>
 {
 public:
 	virtual int B_F() = 0;
-	int B() { return B_F(); }
+	int F() { return B_F(); }
 };
 
 //
@@ -68,9 +57,55 @@ public:
 	int A_F() { return 1; }
 	int B_F() { return 2; }
 };
+*/
+
+struct IUnknown { virtual void QueryInterface() = 0; };
+
+struct A: IUnknown
+{
+	virtual int F() = 0;
+};
+
+struct B: IUnknown
+{
+	virtual int F() = 0;
+};
+
+struct base
+{
+	void query_interface() {}
+};
+
+template<class T, class Base = T>
+struct wrap: Base, virtual base
+{
+	void QueryInterface() { base::query_interface(); }
+};
+
+template<class Base>
+struct wrap<A, Base>: wrap<IUnknown, Base>
+{
+	virtual int A_F() = 0;
+	int F() { return A_F(); }
+};
+
+template<class Base>
+struct wrap<B, Base>: wrap<IUnknown, Base>
+{
+	virtual int B_F() = 0;
+	int F() { return B_F(); }
+};
+
+struct I: wrap<A>, wrap<B> 
+{
+	int A_F() { return 1; }
+	int B_F() { return 1; }
+};
 
 int main()
 {
 	I i;
+	A &a = static_cast<A &>(i);
+	B &b = static_cast<B &>(i);
 	return 0;
 }
