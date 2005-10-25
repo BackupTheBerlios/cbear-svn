3f071efc74a99d3d28d5ddbf5df06e31feecd9a2
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2005-10-25 05:38:55 +0000

    API example was fixed.

--- trunk/cbear.berlios.de/api/cbear_berlios_de_api_example.hpp
+++ trunk/cbear.berlios.de/api/cbear_berlios_de_api_example.hpp
@@ -1,11 +1,23 @@
 #ifndef cbear_berlios_de_api_example_hpp_included
 #define cbear_berlios_de_api_example_hpp_included
 #include <cbear_berlios_de_api_example.h>
+#include <cbear.berlios.de/com/enum.hpp>
+#include <cbear.berlios.de/com/variant_bool.hpp>
+#include <cbear.berlios.de/com/int.hpp>
+#include <cbear.berlios.de/com/byte.hpp>
+#include <cbear.berlios.de/com/ushort.hpp>
+#include <cbear.berlios.de/com/ulong.hpp>
+#include <cbear.berlios.de/com/double.hpp>
+#include <cbear.berlios.de/com/bstr.hpp>
+#include <cbear.berlios.de/com/date.hpp>
+#include <cbear.berlios.de/com/safearray.hpp>
 #include <cbear.berlios.de/com/object.hpp>
+#include <cbear.berlios.de/com/exception.hpp>
+#include <cbear.berlios.de/com/implementation.hpp>
 namespace cbear_berlios_de_api_example
 {
 	class Mode:
-		public ::cbear_berlios_de::policy::wrap< Mode, ::Mode >
+		public ::cbear_berlios_de::com::enum_t< Mode, ::Mode >
 	{
 	public:
 		enum type
@@ -13,11 +25,13 @@ namespace cbear_berlios_de_api_example
 			Disabled = 0,
 			Enabled = 1,
 		};
-		Mode();
-		Mode(::Mode X): ::cbear_berlios_de::policy::wrap< Mode, ::Mode >(X)
+		Mode()
 		{
 		}
-		Mode(type X): ::cbear_berlios_de::policy::wrap< Mode, ::Mode >(X)
+		Mode(::Mode X): ::cbear_berlios_de::com::enum_t< Mode, ::Mode >(X)
+		{
+		}
+		Mode(type X): ::cbear_berlios_de::com::enum_t< Mode, ::Mode >(::Mode(X))
 		{
 		}
 	};
@@ -35,100 +49,281 @@ namespace cbear_berlios_de
 	namespace com
 	{
 		template<class Base>
-		class object_content< ::ITime, Base >:
-			public object_content< ::IDispatch, Base >
+		class object_content< Base, ::ITime >:
+			public object_content< Base, ::IUnknown >
 		{
 		public:
-			date_t get_Date()
+			date_t Date() const
 			{
-				_result;
-				exception::handle(this->internal_this().get_Date());
+				date_t _result;
+				exception::throw_unless(this->internal_reference().get_Date(com::internal< out >(_result)));
+				return _result;
 			}
-			uint_t get_Clock()
+			uint_t Clock() const
 			{
-				_result;
-				exception::handle(this->internal_this().get_Clock());
+				uint_t _result;
+				exception::throw_unless(this->internal_reference().get_Clock(com::internal< out >(_result)));
+				return _result;
 			}
 		};
 		template<class Base>
-		class object_content< ::IDeviceInfo, Base >:
-			public object_content< ::IDispatch, Base >
+		class implementation< Base, ::ITime >:
+			public implementation_base< Base, ::ITime, ::IUnknown >
 		{
 		public:
-			bstr_t get_Name()
+			virtual date_t ITime_Date() = 0;
+			hresult::internal_type __stdcall get_Date(internal_result< out, date_t >::type _result)
 			{
-				_result;
-				exception::handle(this->internal_this().get_Name());
+				try
+				{
+					wrap< out, date_t >(_result)=this->ITime_Date();
+					return hresult::s_ok;
+				}
+				catch(...)
+				{
+					return exception::catch_().internal();
+				}
+			}
+			virtual uint_t ITime_Clock() = 0;
+			hresult::internal_type __stdcall get_Clock(internal_result< out, uint_t >::type _result)
+			{
+				try
+				{
+					wrap< out, uint_t >(_result)=this->ITime_Clock();
+					return hresult::s_ok;
+				}
+				catch(...)
+				{
+					return exception::catch_().internal();
+				}
 			}
 		};
 		template<class Base>
-		class object_content< ::IModuleInfo, Base >:
-			public object_content< ::IDeviceInfo, Base >
-		{
-		};
-		template<class Base>
-		class object_content< ::IModuleInfoList, Base >:
-			public object_content< ::IDispatch, Base >
-		{
-		public:
-			long_t get_Count()
-			{
-				_result;
-				exception::handle(this->internal_this().get_Count());
-			}
-			cbear_berlios_de_api_example::IModuleInfo get_Item(long_t _1)
-			{
-				_result;
-				_result;
-				exception::handle(this->internal_this().get_Item());
-			}
-			void Update()
-			{
-				exception::handle(this->internal_this().Update());
-			}
-		};
-		template<class Base>
-		class object_content< ::IMain, Base >:
-			public object_content< ::IDispatch, Base >
+		class object_content< Base, ::IDeviceInfo >:
+			public object_content< Base, ::IUnknown >
 		{
 		public:
-			Create(bstr_t _1)
+			bstr_t Name() const
 			{
-				_result;
-				_result;
-				exception::handle(this->internal_this().Create());
-			}
-			safearray_t< byte_t > Reserved(safearray_t< byte_t > _1)
-			{
-				_result;
-				_result;
-				exception::handle(this->internal_this().Reserved());
-			}
-			cbear_berlios_de_api_example::IModuleInfoList get_A()
-			{
-				_result;
-				exception::handle(this->internal_this().get_A());
+				bstr_t _result;
+				exception::throw_unless(this->internal_reference().get_Name(com::internal< out >(_result)));
+				return _result;
 			}
 		};
 		template<class Base>
-		class object_content< ::IA, Base >:
-			public object_content< ::IDispatch, Base >
-		{
-		};
-		template<class Base>
-		class object_content< ::IB, Base >:
-			public object_content< ::IDispatch, Base >
-		{
-		};
-		template<class Base>
-		class object_content< ::IC, Base >:
-			public object_content< ::IA, Base >
+		class implementation< Base, ::IDeviceInfo >:
+			public implementation_base< Base, ::IDeviceInfo, ::IUnknown >
 		{
 		public:
-			cbear_berlios_de_api_example::IB get_IB()
+			virtual bstr_t IDeviceInfo_Name() = 0;
+			hresult::internal_type __stdcall get_Name(internal_result< out, bstr_t >::type _result)
 			{
-				_result;
-				exception::handle(this->internal_this().get_IB());
+				try
+				{
+					wrap< out, bstr_t >(_result)=this->IDeviceInfo_Name();
+					return hresult::s_ok;
+				}
+				catch(...)
+				{
+					return exception::catch_().internal();
+				}
+			}
+		};
+		template<class Base>
+		class object_content< Base, ::IModuleInfo >:
+			public object_content< Base, ::IDeviceInfo >
+		{
+		};
+		template<class Base>
+		class implementation< Base, ::IModuleInfo >:
+			public implementation_base< Base, ::IModuleInfo, ::IDeviceInfo >
+		{
+		};
+		template<class Base>
+		class object_content< Base, ::IModuleInfoList >:
+			public object_content< Base, ::IUnknown >
+		{
+		public:
+			long_t Count() const
+			{
+				long_t _result;
+				exception::throw_unless(this->internal_reference().get_Count(com::internal< out >(_result)));
+				return _result;
+			}
+			cbear_berlios_de_api_example::IModuleInfo Item(const long_t & _0) const
+			{
+				cbear_berlios_de_api_example::IModuleInfo _result;
+				exception::throw_unless(this->internal_reference().get_Item(com::internal< in >(_0), com::internal< out >(_result)));
+				return _result;
+			}
+			void Update() const
+			{
+				exception::throw_unless(this->internal_reference().Update());
+			}
+		};
+		template<class Base>
+		class implementation< Base, ::IModuleInfoList >:
+			public implementation_base< Base, ::IModuleInfoList, ::IUnknown >
+		{
+		public:
+			virtual long_t IModuleInfoList_Count() = 0;
+			hresult::internal_type __stdcall get_Count(internal_result< out, long_t >::type _result)
+			{
+				try
+				{
+					wrap< out, long_t >(_result)=this->IModuleInfoList_Count();
+					return hresult::s_ok;
+				}
+				catch(...)
+				{
+					return exception::catch_().internal();
+				}
+			}
+			virtual cbear_berlios_de_api_example::IModuleInfo IModuleInfoList_Item(const long_t & _0) = 0;
+			hresult::internal_type __stdcall get_Item(internal_result< in, long_t >::type _0, internal_result< out, cbear_berlios_de_api_example::IModuleInfo >::type _result)
+			{
+				try
+				{
+					wrap< out, cbear_berlios_de_api_example::IModuleInfo >(_result)=this->IModuleInfoList_Item(wrap< in, long_t >(_0));
+					return hresult::s_ok;
+				}
+				catch(...)
+				{
+					return exception::catch_().internal();
+				}
+			}
+			virtual void IModuleInfoList_Update() = 0;
+			hresult::internal_type __stdcall Update()
+			{
+				try
+				{
+					this->IModuleInfoList_Update();
+					return hresult::s_ok;
+				}
+				catch(...)
+				{
+					return exception::catch_().internal();
+				}
+			}
+		};
+		template<class Base>
+		class object_content< Base, ::IMain >:
+			public object_content< Base, ::IUnknown >
+		{
+		public:
+			cbear_berlios_de_api_example::IModuleInfo Create(const bstr_t & _0) const
+			{
+				cbear_berlios_de_api_example::IModuleInfo _result;
+				exception::throw_unless(this->internal_reference().Create(com::internal< in >(_0), com::internal< out >(_result)));
+				return _result;
+			}
+			safearray_t< byte_t > Reserved(const safearray_t< byte_t > & _0) const
+			{
+				safearray_t< byte_t > _result;
+				exception::throw_unless(this->internal_reference().Reserved(com::internal< in >(_0), com::internal< out >(_result)));
+				return _result;
+			}
+			cbear_berlios_de_api_example::IModuleInfoList A() const
+			{
+				cbear_berlios_de_api_example::IModuleInfoList _result;
+				exception::throw_unless(this->internal_reference().get_A(com::internal< out >(_result)));
+				return _result;
+			}
+		};
+		template<class Base>
+		class implementation< Base, ::IMain >:
+			public implementation_base< Base, ::IMain, ::IUnknown >
+		{
+		public:
+			virtual cbear_berlios_de_api_example::IModuleInfo IMain_Create(const bstr_t & _0) = 0;
+			hresult::internal_type __stdcall Create(internal_result< in, bstr_t >::type _0, internal_result< out, cbear_berlios_de_api_example::IModuleInfo >::type _result)
+			{
+				try
+				{
+					wrap< out, cbear_berlios_de_api_example::IModuleInfo >(_result)=this->IMain_Create(wrap< in, bstr_t >(_0));
+					return hresult::s_ok;
+				}
+				catch(...)
+				{
+					return exception::catch_().internal();
+				}
+			}
+			virtual safearray_t< byte_t > IMain_Reserved(const safearray_t< byte_t > & _0) = 0;
+			hresult::internal_type __stdcall Reserved(internal_result< in, safearray_t< byte_t > >::type _0, internal_result< out, safearray_t< byte_t > >::type _result)
+			{
+				try
+				{
+					wrap< out, safearray_t< byte_t > >(_result)=this->IMain_Reserved(wrap< in, safearray_t< byte_t > >(_0));
+					return hresult::s_ok;
+				}
+				catch(...)
+				{
+					return exception::catch_().internal();
+				}
+			}
+			virtual cbear_berlios_de_api_example::IModuleInfoList IMain_A() = 0;
+			hresult::internal_type __stdcall get_A(internal_result< out, cbear_berlios_de_api_example::IModuleInfoList >::type _result)
+			{
+				try
+				{
+					wrap< out, cbear_berlios_de_api_example::IModuleInfoList >(_result)=this->IMain_A();
+					return hresult::s_ok;
+				}
+				catch(...)
+				{
+					return exception::catch_().internal();
+				}
+			}
+		};
+		template<class Base>
+		class object_content< Base, ::IA >:
+			public object_content< Base, ::IUnknown >
+		{
+		};
+		template<class Base>
+		class implementation< Base, ::IA >:
+			public implementation_base< Base, ::IA, ::IUnknown >
+		{
+		};
+		template<class Base>
+		class object_content< Base, ::IB >:
+			public object_content< Base, ::IUnknown >
+		{
+		};
+		template<class Base>
+		class implementation< Base, ::IB >:
+			public implementation_base< Base, ::IB, ::IUnknown >
+		{
+		};
+		template<class Base>
+		class object_content< Base, ::IC >:
+			public object_content< Base, ::IA >
+		{
+		public:
+			cbear_berlios_de_api_example::IB IB() const
+			{
+				cbear_berlios_de_api_example::IB _result;
+				exception::throw_unless(this->internal_reference().get_IB(com::internal< out >(_result)));
+				return _result;
+			}
+		};
+		template<class Base>
+		class implementation< Base, ::IC >:
+			public implementation_base< Base, ::IC, ::IA >
+		{
+		public:
+			virtual cbear_berlios_de_api_example::IB IC_IB() = 0;
+			hresult::internal_type __stdcall get_IB(internal_result< out, cbear_berlios_de_api_example::IB >::type _result)
+			{
+				try
+				{
+					wrap< out, cbear_berlios_de_api_example::IB >(_result)=this->IC_IB();
+					return hresult::s_ok;
+				}
+				catch(...)
+				{
+					return exception::catch_().internal();
+				}
 			}
 		};
 	}
--- trunk/cbear.berlios.de/api/test.bat
+++ trunk/cbear.berlios.de/api/test.bat
@@ -29,6 +29,9 @@ c:\python24\scripts\4xslt --ignore --noxinclude --outfile=example.odl.xml --defi
 c:\python24\scripts\4xslt --ignore --noxinclude --outfile=example.odl example.odl.xml ../com/odl.xsl
 ..\xsd\bin\Debug\xsd.exe example.odl.xml
 
+call "C:\Program Files\Microsoft Visual C++ Toolkit 2003\vcvars32.bat" 
+"C:\Program Files\Microsoft Platform SDK\Bin\midl.exe" example.odl
+
 rem CPP file.
 rem c:\python24\scripts\4xslt --ignore --noxinclude --outfile=example.com.cpp.xml --define=api:com.cpp.xsd="../cpp/main.xsd" --define=api:com.cpp.xsl="../cpp/html.xsl" --define=api:com.cpp.name="_example.com" example.xml com.cpp.xsl
 rem c:\python24\scripts\4xslt --ignore --noxinclude --define=cpp:cpp.root="../../" example.com.cpp.xml ../cpp/cpp.xsl
