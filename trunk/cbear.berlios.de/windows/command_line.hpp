361a164cabc630a50d278b7d36f81d64ab78e8a2
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2005-11-29 00:36:53 +0000

    minor changes.

--- trunk/cbear.berlios.de/windows/command_line.hpp
+++ trunk/cbear.berlios.de/windows/command_line.hpp
@@ -24,6 +24,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define CBEAR_BERLIOS_DE_WINDOWS_COMMAND_LINE_HPP_INCLUDED
 
 #include <cbear.berlios.de/windows/lpstr.hpp>
+#include <cbear.berlios.de/windows/select.hpp>
 
 namespace cbear_berlios_de
 {
