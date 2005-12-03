a7aa5462a0c00b5d97699e2f3f6af5f98a4eed59
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2005-12-03 03:07:54 +0000

    Base: string test.

--- trunk/cbear.berlios.de/base/string.test.cpp
+++ trunk/cbear.berlios.de/base/string.test.cpp
@@ -1,13 +1,20 @@
 #include <cbear.berlios.de/base/string.hpp>
 #include <iostream>
+#include <boost/assert.hpp>
 
 namespace Base = cbear_berlios_de::base;
 
 int main()
 {
-	Base::string A("\0");
-	std::cout << A.size() << std::endl;
-	A = '\0';
+	BOOST_ASSERT(Base::string("\0").size()==1);
+	BOOST_ASSERT(Base::string('\0').size()==1);
+	BOOST_ASSERT(Base::string("\0\0A\0").size()==4);
+	BOOST_ASSERT(Base::string('\0') == "\0");
+	BOOST_ASSERT("\0" == Base::string('\0'));
+	BOOST_ASSERT(Base::string("\0") == '\0');
+	BOOST_ASSERT('\0' == Base::string("\0"));
+	BOOST_ASSERT(Base::string('\0') == Base::string("\0"));
+	std::cout << "Ok" << std::endl;
 	char C;
 	std::cin.get(C);
 	return 0;
