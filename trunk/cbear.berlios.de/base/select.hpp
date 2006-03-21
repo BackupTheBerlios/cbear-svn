c0c8c00b271b493d3984c0394a7b31a8fa408eb4
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2006-03-21 06:48:31 +0000

    new selection for chars.

--- trunk/cbear.berlios.de/base/select.hpp
+++ trunk/cbear.berlios.de/base/select.hpp
@@ -20,18 +20,52 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-#ifndef CBEAR_BERLIOS_DE_WINDOWS_SELECT_HPP_INCLUDED
-#define CBEAR_BERLIOS_DE_WINDOWS_SELECT_HPP_INCLUDED
+#ifndef CBEAR_BERLIOS_DE_BASE_SELECT_HPP_INCLUDED
+#define CBEAR_BERLIOS_DE_BASE_SELECT_HPP_INCLUDED
 
-#include <cbear.berlios.de/select/base.hpp>
+#include <cbear.berlios.de/pp/widen.hpp>
 
-#define CBEAR_BERLIOS_DE_WINDOWS_SELECT_FUNCTION(Char, Name) \
-	::cbear_berlios_de::base::select<Char>( \
-	BOOST_PP_CAT(Name, A), BOOST_PP_CAT(Name, W))
+namespace cbear_berlios_de
+{
+namespace base
+{
 
-#define CBEAR_BERLIOS_DE_WINDOWS_SELECT_TYPE(Char, Name) \
-	typename ::cbear_berlios_de::select_traits< \
-	Char, BOOST_PP_CAT(Name, A), BOOST_PP_CAT(Name, W)>:: \
-	type
+template<class Char, class A, class W>
+struct select_traits;
+
+template<class A, class W>
+struct select_traits<char, A, W>
+{
+	typedef A type;
+	static type get(A X, W ) { return X; }
+};
+
+template<class A, class W>
+struct select_traits<wchar_t, A, W>
+{
+	typedef W type;
+	static type get(A, W X) { return X; }
+};
+
+template<class Char, class A, class W>
+typename select_traits<Char, A, W>::type select(A a, W w)
+{
+	return select_traits<Char, A, W>::get(a, w);
+}
+
+template<class Char, std::size_t Size>
+typename select_traits<
+	Char, const char (&)[Size], const wchar_t (&)[Size]>::type
+select(const char (&a)[Size], const wchar_t (&w)[Size])
+{
+	return select_traits<
+		Char, const char (&)[Size], const wchar_t (&)[Size]>::get(a, w);
+}
+
+}
+}
+
+#define CBEAR_BERLIOS_DE_BASE_SELECT_STRING(Char, X) \
+	::cbear_berlios_de::base::select<Char>(X, CBEAR_BERLIOS_DE_PP_WIDEN(X))
 
 #endif
