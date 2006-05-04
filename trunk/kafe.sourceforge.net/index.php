b39b104c237b1c0b7829f81e17f8b7dd6bf6a207
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2006-05-04 05:38:28 +0000

    Cool PHP :-) I'm just learning it.

--- trunk/kafe.sourceforge.net/index.php
+++ trunk/kafe.sourceforge.net/index.php
@@ -1,5 +1,12 @@
 <?php 
-	$dir = "http://cbear.berlios.de/" + getcwd();
+foreach($_SERVER as $i => $value)
+{
+	echo "[$i] $value <br/>";
+}
+/*
+	$link = getcwd();
+	$dir = "http://cbear.berlios.de/?" . $link;
 	header("Location: $dir");
 	exit; 
+*/
 ?>
