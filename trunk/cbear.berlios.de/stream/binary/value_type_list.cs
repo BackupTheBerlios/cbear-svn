8fdf6f7d8a6cc9678918bbb03dcff3f75ef6837d
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2006-05-29 11:54:41 +0000

    On C# project!

---
+++ trunk/cbear.berlios.de/stream/binary/value_type_list.cs
@@ -0,0 +1,18 @@
+namespace cbear_berlios_de.stream.binary
+{
+    class value_type_list
+    {
+        public readonly static dynamic[] dynamic =
+        {
+            new @byte(),
+            new @sbyte(),
+            new uint16(),
+            new int16(),
+            new uint32(),
+            new int32(),
+            new uint64(),
+            new int64(),
+        };
+    }
+}
+
