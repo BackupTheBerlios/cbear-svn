bd16b3714f61cae81bab57af1b5a562320493aac
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2006-05-29 09:59:08 +0000

    C#: Binary Streams.

---
+++ trunk/cbear.berlios.de/stream/binary/uint32.cs
@@ -0,0 +1,33 @@
+namespace cbear_berlios_de.stream.binary
+{
+    using IO = System.IO;
+
+    public class uint32 : dynamic_implementation<System.UInt32>
+    {
+        public static System.UInt32 static_read(IO.Stream Stream)
+        {
+            @base.uint32 X;
+            X.low = @uint16.static_read(Stream);
+            X.high = @uint16.static_read(Stream);
+            return X;
+        }
+
+        public static void static_write(IO.Stream Stream, System.UInt32 Value)
+        {
+            @base.uint32 X = Value;
+            @uint16.static_write(Stream, X.low);
+            @uint16.static_write(Stream, X.high);
+        }
+
+        protected override System.UInt32 read(System.IO.Stream Stream)
+        {
+            return static_read(Stream);
+        }
+
+        protected override void write(
+            System.IO.Stream Stream, System.UInt32 Value)
+        {
+            static_write(Stream, Value);
+        }
+    }
+}
