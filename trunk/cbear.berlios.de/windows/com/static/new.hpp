18a14c68c80218ec60fddec8befbfcef95b82610
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2006-06-06 02:21:34 +0000

    Static implementation.

--- trunk/cbear.berlios.de/windows/com/static/new.hpp
+++ trunk/cbear.berlios.de/windows/com/static/new.hpp
@@ -20,8 +20,8 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-#ifndef CBEAR_BERLIOS_DE_WINDOWS_COM_STATIC_IMPLEMENTATION_HPP_INCLUDED
-#define CBEAR_BERLIOS_DE_WINDOWS_COM_STATIC_IMPLEMENTATION_HPP_INCLUDED
+#ifndef CBEAR_BERLIOS_DE_WINDOWS_COM_STATIC_NEW_HPP_INCLUDED
+#define CBEAR_BERLIOS_DE_WINDOWS_COM_STATIC_NEW_HPP_INCLUDED
 
 namespace cbear_berlios_de
 {
@@ -32,29 +32,54 @@ namespace com
 namespace static_
 {
 
-namespace detail
-{
-
-template<class T, class L>
-class traits
+template<class T>
+class implementation
 {
 public:
-	typedef object<T> new_t;
+	implementation() {}
+	implementation(const P &P_) {}
+	void clear() throw()
+	{
+	}
 };
 
+template<class T>
+class owner
+{
+public:
+	typedef implementation<T> implementation_t;
+	typedef pointer<implementation_t> pointer_t;
+	~owner()
+	{
+		this->clear();
+	}
+	void clear() throw()
+	{
+		if(this->P) 
+		{
+			this->P->clear();
+			this->P = pointer_t();
+		}
+	}
+private:
+	pointer_t P;	
+};
+
+template<class T>
+class new_t: public meta::identity<pointer<implementation<T> > > {};
+
+template<class T>
+typename new_t<T>::type::move_t new_()
+{
+	typedef typename new_t<T>::type::interface_t interface_t;
+	return base::move_copy(type::cpp_in(new interface_t()));
 }
 
-template<class T, class L>
-class new_t
+template<class T, class P>
+typename new_t<T>::type::move_t new_(const P &P_)
 {
-public:
-	typedef void type;
-};
-
-template<class T, class L, class P>
-typename new_t<T, L>::type new_(const P &Param)
-{
-	return new new_t<T, L>::type(Param);
+	typedef typename new_t<T>::type::interface_t interface_t;
+	return base::move_copy(type::cpp_in(new interface_t(P_)));
 }
 
 }
