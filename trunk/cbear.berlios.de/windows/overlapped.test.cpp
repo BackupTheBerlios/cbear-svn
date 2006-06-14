1e6086c6eccf828559151e12a7fd87165b1b424b
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2006-06-14 02:52:57 +0000

    Windows: Overlapped: Test.

--- trunk/cbear.berlios.de/windows/overlapped.test.cpp
+++ trunk/cbear.berlios.de/windows/overlapped.test.cpp
@@ -20,36 +20,8 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-#ifndef CBEAR_BERLIOS_DE_WINDOWS_OVERLAPPED_HPP_INCLUDED
-#define CBEAR_BERLIOS_DE_WINDOWS_OVERLAPPED_HPP_INCLUDED
+#include <cbear.berlios.de/windows/overlapped.hpp>
 
-// VC: winnt.h.
-#include <windows.h>
-// MINGW: winnt.h: CONST
-#include <windef.h>
-// MINGW: psapi.h: LPVOID
-#include <winnt.h>
-// DMC: psapi.h: SIZE_T
-#include <basetsd.h>
-#include <psapi.h>
-
-#include <cbear.berlios.de/windows/base.hpp>
-#include <cbear.berlios.de/windows/exception.hpp>
-
-namespace cbear_berlios_de
+int main()
 {
-namespace windows
-{
-
-/*
-class overlapped: public policy::wrap<overlapped, ::OVERLAPPED>
-{
-};
-*/
-
-typedef base::initialized< ::OVERLAPPED> overlapped;
-
 }
-}
-
-#endif
