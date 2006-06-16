9c71b9ae8001e885bc409bf6ac6094fdda36dd32
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2006-06-16 15:33:09 +0000

    Test.

--- trunk/cbear.berlios.de/test/main.bat
+++ trunk/cbear.berlios.de/test/main.bat
@@ -1,2 +1,3 @@
-c:\nxslt-1.6.4\bin\nxslt main.xml main.xsl -o _test.bat
+c:\nxslt-1.6.4\bin\nxslt main.xml main.xsl -o _test.bat.xml
+c:\nxslt-1.6.4\bin\nxslt _test.bat.xml ../bat/main.xsl -o _test.bat
 call _test.bat
