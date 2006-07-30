e72a725f94bb04a427e895fb386c1015d8e21404
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2006-07-30 06:59:31 +0000

    License text has been removed from the source files.

--- trunk/cbear.berlios.de/api/test.bat
+++ trunk/cbear.berlios.de/api/test.bat
@@ -1,25 +1,3 @@
-@rem The MIT License
-@rem
-@rem Copyright (c) 2005 C Bear (http://cbear.berlios.de)
-@rem 
-@rem Permission is hereby granted, free of charge, to any person obtaining a 
-@rem copy of this software and associated documentation files (the "Software"),
-@rem to deal in the Software without restriction, including without limitation 
-@rem the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-@rem and/or sell copies of the Software, and to permit persons to whom the 
-@rem Software is furnished to do so, subject to the following conditions:
-@rem 
-@rem The above copyright notice and this permission notice shall be included in 
-@rem all copies or substantial portions of the Software.
-@rem 
-@rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-@rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-@rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-@rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-@rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-@rem FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-@rem DEALINGS IN THE SOFTWARE.
-
 ..\xsd\bin\Debug\xsd.exe base.xml
 ..\xsd\bin\Debug\xsd.exe example.xml
 
