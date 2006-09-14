457e2165265c995fc42b3128d148278ac57d69e2
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2006-09-14 06:27:05 +0000

    minidump: unhandled exceptions.

--- trunk/cbear.berlios.de/windows/minidump.hpp
+++ trunk/cbear.berlios.de/windows/minidump.hpp
@@ -27,15 +27,11 @@ template<class T>
 class minidump
 {
 protected:
-	static ::std::wstring FileName;
-	static ::LPTOP_LEVEL_EXCEPTION_FILTER Prior;
+	static std::wstring FileName;
 };
 
 template<class T>
-::std::wstring minidump<T>::FileName;
-
-template<class T>
-::LPTOP_LEVEL_EXCEPTION_FILTER minidump<T>::Prior;
+std::wstring minidump<T>::FileName;
 
 }
 
@@ -45,10 +41,10 @@ public:
 	static void init(hmodule Module)
 	{
 		FileName = Module.file_name<wchar_t>() + L".dmp";
-		Prior = ::SetUnhandledExceptionFilter(&filter);
+		::SetUnhandledExceptionFilter(&filter);
 	}
 private:
-	static long_t __stdcall filter(::EXCEPTION_POINTERS *ExceptionPointers)
+	static long_t __stdcall filter(EXCEPTION_POINTERS *ExceptionPointers)
 	{
 		::MINIDUMP_EXCEPTION_INFORMATION ExInfo;
 		ExInfo.ThreadId = ::GetCurrentThreadId();
@@ -62,28 +58,23 @@ private:
 			CREATE_ALWAYS, 
 			FILE_ATTRIBUTE_NORMAL, 
 			NULL);
-		if(hFile != INVALID_HANDLE_VALUE)
-		{
-			if(::MiniDumpWriteDump(
-				::GetCurrentProcess(), 
-				::GetCurrentProcessId(), 
-				hFile, 
-				MiniDumpNormal, 
-				&ExInfo, 
-				0, 
-				0))
-			{
-				CloseHandle(hFile);
-			}
-		}
-		if(Prior)
-		{
-			return (*Prior)(ExceptionPointers);
-		}
-		else
+		if(hFile==INVALID_HANDLE_VALUE) 
 		{
 			return EXCEPTION_CONTINUE_SEARCH;
 		}
+		if(!::MiniDumpWriteDump(
+			::GetCurrentProcess(), 
+			::GetCurrentProcessId(), 
+			hFile, 
+			MiniDumpNormal, 
+			&ExInfo, 
+			0, 
+			0)) 
+		{
+			return EXCEPTION_CONTINUE_SEARCH;
+		}
+		CloseHandle(hFile);
+		return EXCEPTION_CONTINUE_SEARCH;
 	}
 };
 
