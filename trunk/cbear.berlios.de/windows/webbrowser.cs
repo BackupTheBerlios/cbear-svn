869626bbab4ccf3924bb6f7a29a214bad9108b96
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2006-10-10 01:20:06 +0000

    .NET key.

--- trunk/cbear.berlios.de/windows/webbrowser.cs
+++ trunk/cbear.berlios.de/windows/webbrowser.cs
@@ -4,6 +4,10 @@ namespace cbear_berlios_de.windows
 
 	public class webbrowser
 	{
+		private webbrowser()
+		{
+		}
+
 		public static void wait(F.WebBrowser @this)
 		{
 			@this.DocumentText = null;
@@ -19,6 +23,27 @@ namespace cbear_berlios_de.windows
 			F.HtmlElement element = parent.Document.CreateElement(tag);
 			parent.AppendChild(element);
 			return element;
+		}
+	}
+
+	public class treeview
+	{
+		private treeview()
+		{
+		}
+
+		public delegate bool find_delegate(F.TreeNode node);
+
+		public static F.TreeNode find(F.TreeNodeCollection collection, find_delegate d)
+		{
+			foreach (F.TreeNode n in collection)
+			{
+				if (d(n))
+				{
+					return n;
+				}
+			}
+			return null;
 		}
 	}
 }
