4cc68fb08c39c33eae3b962120eca307008fa4fb
sergey_shandar <sergey_shandar@e6e9985e-9100-0410-869a-e199dc1b6838>
2007-01-17 00:22:24 +0000

    windows.hwnd.Show.

--- trunk/cbear.berlios.de/windows/hwnd.hpp
+++ trunk/cbear.berlios.de/windows/hwnd.hpp
@@ -359,6 +359,66 @@ public:
 		exception::scope_last_error ScopedLastError;
 		::PostQuitMessage(ExitCode);
 	}
+
+	enum cmd_show_t
+	{
+		// Windows 2000/XP: Minimizes a window, even if the thread that owns the 
+		// window is not responding. This flag should only be used when minimizing 
+		// windows from a different thread.
+		forceminimize = SW_FORCEMINIMIZE,
+    
+		// Hides the window and activates another window.
+		hide = SW_HIDE,
+    
+		// Maximizes the specified window.
+		maximize = SW_MAXIMIZE,
+    
+		// Minimizes the specified window and activates the next top-level window 
+		// in the Z order.
+		minimize = SW_MINIMIZE,
+    
+		// Activates and displays the window. If the window is minimized or 
+		// maximized, the system restores it to its original size and position. An 
+		// application should specify this flag when restoring a minimized window.
+		restore = SW_RESTORE,
+    
+		// Activates the window and displays it in its current size and position. 
+		show = SW_SHOW,
+    
+		// Sets the show state based on the SW_ value specified in the STARTUPINFO 
+		// structure passed to the CreateProcess function by the program that 
+		// started the application. 
+		showdefault = SW_SHOWDEFAULT,
+    
+		// Activates the window and displays it as a maximized window.
+		showmaximized = SW_SHOWMAXIMIZED,
+    
+		// Activates the window and displays it as a minimized window.
+		showminimized = SW_SHOWMINIMIZED,
+    
+		// Displays the window as a minimized window. This value is similar to 
+		// SW_SHOWMINIMIZED, except the window is not activated.
+		showminnoactive = SW_SHOWMINNOACTIVE,
+    
+		// Displays the window in its current size and position. This value is 
+		// similar to SW_SHOW, except the window is not activated.
+		showna = SW_SHOWNA,
+    
+		// Displays a window in its most recent size and position. This value is 
+		// similar to SW_SHOWNORMAL, except the window is not actived.
+		shownoactive = SW_SHOWNOACTIVATE, 
+    
+		// Activates and displays a window. If the window is minimized or maximized,
+		// the system restores it to its original size and position. An application 
+		// should specify this flag when displaying the window for the first time.
+		shownormal = SW_SHOWNORMAL,    
+	};
+
+	bool Show(cmd_show_t CmdShow) const
+	{
+		exception::scope_last_error ScopeLastError;
+		return ::ShowWindow(this->get(), CmdShow) != FALSE;
+	}
 };
 
 }
